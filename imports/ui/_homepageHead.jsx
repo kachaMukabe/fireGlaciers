6536f71030da5ef67ca1d676d32868f2b5edd42c
lushomo chimimba <lushomochimimba9@gmail.com>
2018-10-20 17:28:14 +0200

    added bootsrap and a _homePageHead file

---
+++ imports/ui/_homepageHead.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Forecast extends Component {
+
+
+    render() {
+        return (
+            <div>
+                {/* {this.showPosition} */}
+            </div>
+        )
+    }
+}
--- imports/ui/mosquitoForecast.jsx
+++ imports/ui/mosquitoForecast.jsx
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 
+
 export class Forecast extends Component {
 
-	componentDidMount() {
-		this.getLocation();
-	}
+    componentDidMount() {
+        this.getLocation();
+    }
 
-	getLocation = () => {
-		if (navigator.geolocation) {
-		  navigator.geolocation.getCurrentPosition(this.showPosition);
-		}
-		else {
-			alert("Geolocation not supported");
-		}
-	}
+    getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition);
+        }
+        else {
+            alert("Geolocation not supported");
+        }
+    }
 
-	showPosition = (position) => {
-		alert("Position: " + position.coords.latitude + ", " + position.coords.longitude)
-	}
+    showPosition = (position) => {
+        alert("Position: " + position.coords.latitude + ", " + position.coords.longitude)
+    }
 
-	render() {
-		return (
-			<div>
-				{/* {this.showPosition} */}
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                {/* {this.showPosition} */}
+            </div>
+        )
+    }
 }
