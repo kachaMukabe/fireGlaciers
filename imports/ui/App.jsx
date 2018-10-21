57abd1e8059905a14a06218f8436108a505859b6
lushomo chimimba <lushomochimimba9@gmail.com>
2018-10-21 13:17:36 +0200

    added aboutcards

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Forecast} from "./mosquitoForecast";
 import { HomepageHead } from "./_homepageHead.jsx"
+import { AboutCards } from "./_aboutCards.jsx"
 
 const App = () => (
   <div>
     <Forecast />
     <HomepageHead/>
+    <AboutCards/>
   </div>
 );
 
