0e3c69c0160ddc5d50387e02e84e4ffac98e78e2
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-10-25 22:31:05 +0200

    Used api to get weather

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import {Forecast} from "./mosquitoForecast";
+import React, {Component} from 'react';
+import Forecast from "./mosquitoForecast";
 import { HomepageHead } from "./_homepageHead.jsx"
 import { AboutCards } from "./_aboutCards.jsx"
 import { SearchInput } from "./_searchInput.jsx"
 
-const App = () => (
-  <div>
-    <Forecast />
-    <HomepageHead/>
-    <AboutCards/>
-    <SearchInput/>
-  </div>
-);
+class App extends Component{
+
+	render(){
+		return(
+			<div>
+				<Forecast />
+		    <HomepageHead/>
+		    <AboutCards/>
+		    <SearchInput/>
+		  </div>
+		)
+	}
+}
 
 export default App;
