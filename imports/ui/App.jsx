60987495cff3f3667ccb6618a77c0317831b228e
lushomo chimimba <lushomochimimba9@gmail.com>
2018-10-28 00:00:45 +0200

    added footer and about section

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,21 +2,20 @@ import React, { Component } from "react";
 import Forecast from "./weather_forecast";
 import { MosquitoGauge } from "./MosquitoGauage";
 import { HomepageHead } from "./_homepageHead.jsx";
-import { AboutCards } from "./_aboutCards.jsx";
+import { About } from "./aboutPage";
 import { SearchInput } from "./_searchInput.jsx";
 import "../../client/index.scss";
+import { Footer } from "./footer.jsx"; 
 
 class App extends Component {
   render() {
-    return (
-      <div>
+    return <div id="AppJs">
         <Forecast />
-        <MosquitoGauge/>
+        <MosquitoGauge />
         <HomepageHead />
-        <AboutCards />
-        <SearchInput />
-      </div>
-    );
+        <About />
+        <Footer />
+      </div>;
   }
 }
 
