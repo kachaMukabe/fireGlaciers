fca6c62edb79fe130cfb5bf37afef9530e239eb8
abednegotm <abednegomwanza@gmail.com>
2018-10-27 23:12:55 +0200

    header and humbuger menu button done

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,16 +5,17 @@ import { HomepageHead } from "./_homepageHead.jsx";
 import { AboutCards } from "./_aboutCards.jsx";
 import { SearchInput } from "./_searchInput.jsx";
 import "../../client/index.scss";
+import MainHeader from "./header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Forecast />
-        <MosquitoGauge/>
-        <HomepageHead />
-        <AboutCards />
-        <SearchInput />
+        <div>
+          <MainHeader />
+          <Forecast />
+        </div>
+        <MosquitoGauge />
       </div>
     );
   }
---
+++ imports/ui/header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class MainHeader extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header ">
+          <h3 className="hd-txt ">Mosquitoe Forecast</h3>
+          <img
+            onClick={() => {
+              alert("menu coming sooner before sunday midnight kwati");
+            }}
+            className="hmb-menu "
+            src="/images/humb.png"
+            alt="humburger menu"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainHeader;
